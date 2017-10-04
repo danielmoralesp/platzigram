a7754dd00a43133a6380b36dfe911db4e4589261
danielmoralesp <daniel_moralesp@hotmail.com>
2017-10-03 20:56:58 -0500

    Add Page.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-alert('Hola');
+var page = require('page');
+
+var main = document.getElementById('main-container')
+
+page('/', function(ctx, next){
+  main.innerHTML = 'Home <a href="/signup">Signup</a>' // Home
+})
+
+page('/signup', function(ctx, next){
+  main.innerHTML = 'Signup <a href="/">Home</a>' // signup
+})
+
+page();
