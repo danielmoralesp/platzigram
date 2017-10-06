fe5c347d555419cf04475f83795ca774d6feb129
danielmoralesp <daniel_moralesp@hotmail.com>
2017-10-06 17:22:40 -0500

    Agregando interfaces de usuario con yo-yo

--- src/index.js
+++ src/index.js
@@ -1,13 +1,60 @@
 var page = require('page');
+var yo = require('yo-yo');
+var empty = require('empty-element');
 
 var main = document.getElementById('main-container')
 
 page('/', function(ctx, next){
-  main.innerHTML = 'Home <a href="/signup">Signup</a>' // Home
+  main.innerHTML = '<a href="/signup">Signup</a>'
+
 })
 
 page('/signup', function(ctx, next){
-  main.innerHTML = 'Signup 2 <a href="/">Home</a>' // signup
+  var el = yo`<div class="container">
+    <div class="row">
+      <div class="col s10 push-s1">
+        <div class="row">
+          <div class="col m5 hide-on-small-only">
+            <img src="public/iphone.png" alt="" class="iphone">
+          </div>
+          <div class="col s12 m7">
+            <div class="row">
+              <div class="signup-box">
+                <h1 class="platzigram">Platzigram</h1>
+                <form action="" class="signup-form">
+                  <h2>Registrate para ver fotos de tu amigos estudiando en Platzi</h2>
+                  <div class="section">
+                    <a href="" class="btn btn-fb hide-on-small-only">
+                      Iniciar sesion con Facebook
+                    </a>
+                    <a href="" class="btn btn-fb hide-on-med-and-up">
+                      Iniciar sesion
+                    </a>
+                  </div>
+                  <div class="divider">
+                  </div>
+                  <div class="section">
+                    <input type="email" name="email" placeholder="Correl electronico">
+                    <input type="text" name="name" placeholder="Nombre completo">
+                    <input type="text" name="username" placeholder="Nombre de usuario">
+                    <input type="password" name="password" placeholder="Contraseña">
+                    <button class="btn waves-effect waves-light btn-signup" type="submit">Regístrate</button>
+                  </div>
+                </form>
+              </div>
+            </div>
+            <div class="row">
+              <div class="login-box">
+                ¿Tienes una cuenta? <a href="/signin">Entrar</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>`;
+
+  empty(main).appendChild(el);
 })
 
 page();
